e39c198fb2d83bb12d82128ac711b7e9b2cb88e1
C0l0ch0 <Alex89Perez@gmail.com>
2022-12-25 22:36:30 -0600

    Adding loader animation

---
+++ JS/Main.js
@@ -0,0 +1,63 @@
+var canvas, pathLength, loaderText, textInterval;
+var textCounter = 0;
+var textIndex = 0;
+
+function startLoader(element, length){
+
+    length -= 5;
+
+    if (length < 0){
+        length = 0; 
+
+        clearInterval(textInterval);
+
+        document.getElementById("lock-animation").classList.remove("fa-lock");
+        document.getElementById("lock-animation").classList.add("fa-lock-open");
+
+        setTimeout( function(){ 
+            document.getElementById("mainLoader").style.opacity = '0'; 
+            document.getElementById("mainLoader").style.display = "none";
+        }, 1000)
+    }
+        
+    element.style.strokeDashoffset = length;
+
+    if (length > 0) {
+        setTimeout(function() { startLoader(element, length); }, 60);
+    }
+
+}
+
+function loaderTextChange(){
+
+    var encriptedText, resultText = "";
+    const welcomeTxt = "Bienvenido";
+    textCounter++;
+
+    if (!(textCounter % 6)){
+        textIndex++;
+        console.log("" + textCounter);
+    }
+
+    encriptedText = (Math.random() + 1).toString(36).slice(2, (12-textIndex)).toUpperCase();
+
+    console.log(welcomeTxt.substring(0, textIndex));
+    resultText =  welcomeTxt.substring(0, textIndex) + encriptedText;
+
+    loaderText.innerText = resultText;
+    
+}
+
+
+document.onreadystatechange = function(){
+    if(document.readyState=='loaded' || document.readyState=='complete'){
+        canvas = document.getElementById("circleLoader");
+        pathLength = canvas.getTotalLength();
+
+        loaderText = document.getElementById("wTextValue");
+
+        textInterval = setInterval(loaderTextChange, 100);
+
+        startLoader(canvas, pathLength);
+    }
+}
